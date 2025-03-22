cd5d41f64235b980cd45d92f89118663b1b30759
Mark Czirr <shutdownemergency@gmail.com>
2025-03-22 15:49:28 -0400

    initial commit

---
+++ app/Enums/CharacterStationEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum CharacterStationEnum: string
+{
+    case Minion = 'minion';
+    case Enforcer = 'enforcer';
+    case Henchman = 'henchman';
+    case Master = 'master';
+}
---
+++ app/Models/Miniature.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Miniature extends Model
+{
+    /** @use HasFactory<\Database\Factories\MiniatureFactory> */
+    use HasFactory;
+}

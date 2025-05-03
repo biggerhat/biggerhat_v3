e4e89041b7f0ab195b799cdab936deacae6c198b
Mark Czirr <77179240+biggerhat@users.noreply.github.com>
2025-05-03 11:43:26 -0400

    fix: fix api functions and add suggested changes (#18)

--- app/Http/Controllers/API/CharacterAPIController.php
+++ app/Http/Controllers/API/CharacterAPIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Character;
 use App\Models\Miniature;
 use Illuminate\Http\Request;
 
@@ -12,10 +13,43 @@ class CharacterAPIController extends Controller
     {
         $name = $request->get('name');
 
-        return Miniature::where('display_name', 'LIKE', "%{$name}%")
+        $miniatures = Miniature::where('display_name', "%{$name}%")
             ->orWhereHas('character', function ($query) use ($name) {
-                $query->where('nicknames', 'LIKE', "%{$name}%");
+                $query->where('nicknames', "%{$name}%");
             })
             ->with('character')->get();
+
+        if (! $miniatures) {
+            $miniatures = Miniature::where('display_name', 'LIKE', "%{$name}%")
+                ->orWhereHas('character', function ($query) use ($name) {
+                    $query->where('nicknames', 'LIKE', "%{$name}%");
+                })
+                ->with('character')->get();
+        }
+
+        return $miniatures;
+    }
+
+    public function images(Request $request)
+    {
+        $storageUrl = config('filesystems.disks.public.url').'/';
+
+        return Character::whereHas('miniatures')
+            ->with('miniatures')
+            ->orderBy('display_name', 'ASC')
+            ->get()
+            ->map(function (Character $character) use ($storageUrl) {
+                return [
+                    'display_name' => $character->display_name,
+                    'front_image' => $storageUrl.$character->miniatures()->first()->front_image,
+                    'back_image' => $storageUrl.$character->miniatures()->first()->back_image,
+                    'combination_image' => $storageUrl.$character->miniatures()->first()->combination_image,
+                    'url' => route('characters.view', [
+                        'character' => $character->slug,
+                        'miniature' => $character->miniatures()->first()->id,
+                        'slug' => $character->miniatures()->first()->slug,
+                    ]),
+                ];
+            });
     }
 }
---
+++ resources/js/pages/Factions/View.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {Head, usePage} from '@inertiajs/vue3';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator';
+import {SharedData} from "@/types";
+import ArcanistLogo from "@/components/ArcanistLogo.vue";
+const page = usePage<SharedData>();
+
+const props = defineProps({
+    faction: {
+        type: [Object, Array],
+        required: false,
+        default() {
+            return null;
+        }
+    },
+    characters: {
+        type: [Object, Array],
+        required: false,
+        default() {
+            return {};
+        }
+    }
+});
+</script>
+
+<template>
+    <Head :title="faction.name" />
+    <div class="w-full h-full">
+        <div class="container mx-auto items-center">
+            <div class="inline-flex items-center justify-center mt-16 w-full">
+                <hr class="bg-primary w-full h-1 my-8 border-0 rounded-sm">
+                <div class="absolute px-4 -translate-x-1/2 left-1/2">
+                    <img src='/images/Factions/M4E-Arcanists.png' class="w-40 h-40" :alt="props.faction.name" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>

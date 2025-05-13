34790412dccabe4e169d543ef5efb17e8e2144bc
Mark Czirr <shutdownemergency@gmail.com>
2025-05-13 09:15:32 -0400

    fix: javascript nonsense

--- resources/js/pages/Factions/View.vue
+++ resources/js/pages/Factions/View.vue
@@ -301,14 +301,14 @@ onMounted(() => {
         </div>
         <div v-if="currentView === 'keyword_breakdown'" class="container mx-auto items-center mt-8">
             <div v-for="keyword in props.keyword_breakdown" v-bind:key="keyword.keyword.name">
-                <div v-if="keyword.characters.length > 0" class="mb-6">
+                <div v-if="Object.keys(keyword.characters).length > 0" class="mb-6">
                     <div class="relative flex py-5 items-center">
                         <div class="flex-grow border-t border-primary"></div>
                         <span class="flex-shrink mx-4 text-lg text-primary">{{ keyword.keyword.name }}</span>
                         <div class="flex-grow border-t border-primary"></div>
                     </div>
                     <div class="w-full lg:grid-cols-6 grid">
-                        <div class="hidden lg:block grid"><CharacterCardView v-if="keyword.masters.length > 0" :miniature="keyword.masters[0]['standard_miniatures'][0]" /></div>
+                        <div class="hidden lg:block grid"><CharacterCardView v-if="Object.keys(keyword.masters).length > 0" :miniature="keyword.masters[0]['standard_miniatures'][0]" /></div>
                         <div class="lg:col-span-4">
                             <div class="w-full grid lg:grid-cols-4">
                                 <div v-for="character in keyword.characters" v-bind:key="character.slug">
@@ -316,7 +316,7 @@ onMounted(() => {
                                 </div>
                             </div>
                         </div>
-                        <div class="hidden lg:block grid"><CharacterCardView v-if="keyword.masters.length > 1" :miniature="keyword.masters[1]['standard_miniatures'][0]" /></div>
+                        <div class="hidden lg:block grid"><CharacterCardView v-if="Object.keys(keyword.masters).length > 1" :miniature="keyword.masters[1]['standard_miniatures'][0]" /></div>
                     </div>
                 </div>
             </div>

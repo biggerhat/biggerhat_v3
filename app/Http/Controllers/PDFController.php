4c78c6c495d8bd58a0e07138a8ea1a0c5c004649
Mark Czirr <shutdownemergency@gmail.com>
2025-05-28 08:06:51 -0400

    feat: add keyword filtering

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Enums\FactionEnum;
 use App\Models\Character;
+use App\Models\Keyword;
 use App\Models\Miniature;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
@@ -13,16 +14,23 @@ class PDFController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Character::with('standardMiniatures');
+        $query = Character::with(['standardMiniatures', 'keywords']);
 
         if ($request->get('faction')) {
             $query->where('faction', $request->faction);
+        }
+
+        if ($request->get('keyword')) {
+            $query->whereHas('keywords', function ($subQuery) use ($request) {
+                $subQuery->where('slug', $request->get('keyword'));
+            });
         }
 
         $characters = $query->whereHas('standardMiniatures')->orderBy('name', 'ASC')->get();
 
         return inertia('PDF/Index', [
             'factions' => FactionEnum::buildDetails(),
+            'keywords' => Keyword::orderBy('name', 'ASC')->get(),
             'characters' => fn () => $characters,
         ]);
     }

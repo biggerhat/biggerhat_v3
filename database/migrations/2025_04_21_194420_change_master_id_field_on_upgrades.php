0c1b34d8f3a0cfd187d8ac9b363ff01376066f9e
Blake Severson <tarsonis21@gmail.com>
2025-05-27 18:04:19 -0700

    chore: linting

--- database/migrations/2025_04_21_194420_change_master_id_field_on_upgrades.php
+++ database/migrations/2025_04_21_194420_change_master_id_field_on_upgrades.php
@@ -4,11 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
-    public function up(): void {
+    public function up(): void
+    {
         Schema::table('upgrades', function (Blueprint $table) {
             $table->dropForeignSafe('upgrades_master_id_foreign');
         });
@@ -23,6 +25,5 @@ return new class extends Migration {
     /**
      * Reverse the migrations.
      */
-    public function down(): void {
-    }
+    public function down(): void {}
 };
